4ad89d830d6eea548091e352dab6e9bf2de23b20
BaiJiFeiLong <baijifeilong@gmail.com>
2021-12-08 13:41:22 +0800

    Reduce package size

--- make.py
+++ make.py
@@ -1,69 +1,40 @@
 # Created by BaiJiFeiLong@gmail.com at 2021/12/7 18:05
+import logging
 import os
-import shutil
 from pathlib import Path
 
 import PyInstaller.__main__
+import colorlog
+
+consolePattern = "%(log_color)s%(asctime)s %(levelname)8s %(name)-10s %(message)s"
+logging.getLogger().handlers = [logging.StreamHandler()]
+logging.getLogger().handlers[0].setFormatter(colorlog.ColoredFormatter(consolePattern))
+logging.getLogger().setLevel(logging.DEBUG)
 
 name = "IceSpringSwaggerConverter"
 
 excluded_files = """
-Qt5DataVisualization.dll
-Qt5Pdf.dll
-Qt5Quick.dll
-Qt5VirtualKeyboard.dll
-d3dcompiler_47.dll
-libGLESv2.dll
-opengl32sw.dll
-""".strip().splitlines()
+Qt5DataVisualization.dll Qt5Pdf.dll Qt5Quick.dll Qt5VirtualKeyboard.dll
+d3dcompiler_47.dll libGLESv2.dll opengl32sw.dll crown.png
+""".strip().split()
 
-print("Building...")
-if Path("dist").exists():
-    print("Folder dist exists, removing...")
-    shutil.rmtree("dist")
+logging.info("Removing dist folder if exists...")
+os.system("busybox rm -rf dist")
+logging.info(f"Removing {name}.7z if exists...")
+os.system(f"busybox rm -rf {name}.7z")
 
-if Path(f"{name}.7z").exists():
-    print("Target archive exists, removing...")
-    Path(f"{name}.7z").unlink()
+command = f"main.py --noconsole --noupx --name {name} --ico resources/crown.ico " \
+          f"--add-data resources;resources --add-data jre;jre --add-data jars;jars"
+logging.info("Executing PyInstaller...")
+PyInstaller.__main__.run(command.split())
 
-print("\nBuilding jar...")
-os.system("mvn -DskipTests package dependency:copy-dependencies")
-
-print("\nPackaging...")
-PyInstaller.__main__.run([
-    "main.py",
-    "--noconsole",
-    "--noupx",
-    "--name",
-    name,
-    "--ico",
-    "resources/crown.ico"
-])
-
-print("\nCopying resources...")
-shutil.copytree("resources", f"dist/{name}/resources")
-
-print("\nRemoving unused resources...")
-for file in Path(f"dist/{name}/resources").glob("**/*"):
-    if file.name in ["crown.png"]:
-        print(f"\tRemoving {file}...")
-        file.unlink()
-
-print("\nCopying JRE...")
-shutil.copytree("jre", f"dist/{name}/jre")
-
-print("\nCopying dependency jars...")
-shutil.copytree("target/dependency", f"dist/{name}/jars")
-
-print("\nCopying main jar...")
-jar = next(Path("target").glob("swagger-converter*.jar"))
-shutil.copy(jar, f"dist/{name}/jars")
-
-print("\nCleaning...")
-for file in Path("dist").glob("*/*"):
+logging.info("Cleaning...")
+for file in Path("dist").glob("**/*"):
     if file.name in excluded_files:
-        print(f"\tRemoving {file.name}...")
+        logging.debug(f"Removing %s...", file.name)
         file.unlink()
 
-print("\nCompressing...")
+logging.info("Compressing...")
 os.system(f"cd dist && 7z a -mx=9 ../{name}.7z {name}")
+
+logging.info("Done.")
